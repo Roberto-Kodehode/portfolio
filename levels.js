c5b381c1c7c1c287eb0077bbe9475feea0a83efb
Roberto Kodehode <roberto.kodehode@gmail.com>
2023-07-25 19:57:27 +0200

    minor changes

--- levels.js
+++ levels.js
@@ -5,6 +5,9 @@ const level = [
     imageSrc: "img/venus.png",
     description:
       "First project at Kodehode! Designing webpage in Figma, then utilze what we had learnt for the...",
+    github: "denne er til Github",
+    linkTitle: "Link tittel",
+    links: "Dette er lenken",
   },
   {
     id: "level-21",
@@ -12,30 +15,45 @@ const level = [
     imageSrc: "img/666.png",
     description:
       "First javascript basic project. bla bla bla, manipulating DOM",
+    github: "denne er til Github",
+    linkTitle: "Link tittel",
+    links: "Dette er lenken",
   },
   {
     id: "level-31",
     name: "PROJECT CHUCK",
     imageSrc: "img/chuck.png",
     description: "javascript advanced. drumkit?",
+    github: "denne er til Github",
+    linkTitle: "Link tittel",
+    links: "Dette er lenken",
   },
   {
     id: "level-41",
     name: "PROJECT PIZZA",
-    imageSrc: "img/placeholder.png",
-    description: "pizza library?",
+    imageSrc: "img/pizza.png",
+    description: "pizza library? local storage",
+    github: "denne er til Github",
+    linkTitle: "Link tittel",
+    links: "Dette er lenken",
   },
   {
     id: "level-51",
     name: "PROJECT DRUM",
-    imageSrc: "img/chuck.png",
+    imageSrc: "img/drumkit.png",
     description: "DRUMKIT BLA BLA BLA",
+    github: "denne er til Github",
+    linkTitle: "Link tittel",
+    links: "Dette er lenken",
   },
   {
     id: "level-boss",
-    name: "PROJECT CALCULATOR",
-    imageSrc: "img/placeholder.png",
-    description: "REACT calculator",
+    name: "PROJECT REACT",
+    imageSrc: "img/calculator.png",
+    description: "A CALCULATOR BUILT IN REACT. USING COMPONENTS AND USESTATE.",
+    github: "denne er til Github",
+    linkTitle: "Calculator",
+    links: "https://chic-strudel-e1153b.netlify.app/",
   },
 ];
 
@@ -44,12 +62,29 @@ const elements = level.map((lvl) => ({
   name: document.getElementById("card-title"),
   image: document.getElementById("card-img"),
   description: document.getElementById("card-description"),
+  github: document.getElementById("github"),
+  links: document.getElementById("live"),
 }));
 
-elements.forEach(({ btn, name, description, image }, index) => {
+elements.forEach(({ btn, name, description, image, github, links }, index) => {
   btn.addEventListener("click", () => {
     name.textContent = level[index].name;
     description.textContent = level[index].description;
     image.src = level[index].imageSrc;
+    github.textContent = level[index].github;
+
+    // Create an anchor element and set its textContent and href attributes
+    const linkAnchor = document.createElement("a");
+    linkAnchor.textContent = level[index].linkTitle;
+    linkAnchor.href = level[index].links;
+    linkAnchor.target = "_self";
+
+    // Clear the existing content of the "links" element before appending the anchor
+    while (links.firstChild) {
+      links.removeChild(links.firstChild);
+    }
+
+    // Append the anchor element to the "links" element
+    links.appendChild(linkAnchor);
   });
 });
